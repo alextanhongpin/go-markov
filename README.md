61d09d96185023876fedaee5712a7ac49b80fba0
alextanhongpin <alextan220990@gmail.com>
2018-11-03 13:12:52 +0800

    docs: create README

---
+++ README.md
@@ -0,0 +1,18 @@
+# go-markov
+
+Text-generation with markov chain. This can be used for several other purposes:
+
+- chat conversation generation
+- quotes/lyrics/speech generation
+
+
+## Run
+
+```bash
+$ cat alice_oz.txt | go run markov.go
+```
+
+Output:
+```
+Woodman very unhappy, answered the Woodman; so do not know of to keep tramping forever, without getting anywhere at all. She stood, with Toto curled up on tiptoe, and peeped over the fire, and at last and stepped off upon the front of them, and find out the proper way of settling all difficulties, great or small. 'Off with her mind to harness him like a star-fish,' thought Alice. 'I've so often read in the road and lay still. Little Toto, now that he is too heavy to lift. We must certainly get to twenty at that rate! However, the Multiplication Table doesn't signify: let's try the experiment?' 'He might bite,' Alice cautiously replied, not feeling at all for any of them about me.
+```
--- markov.go
+++ markov.go
@@ -7,6 +7,9 @@ import (
 	"io"
 	"math/rand"
 	"os"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 // https://rosettacode.org/wiki/Markov_chain_text_generator#Go
@@ -24,6 +27,13 @@ func (p Prefix) String() string {
 func (p Prefix) Shift(b []byte) {
 	copy(p, p[1:])
 	p[len(p)-1] = b
+}
+
+func (p Prefix) ShiftString(s string) {
+	str := strings.Split(s, " ")
+	for _, s := range str {
+		p.Shift([]byte(s))
+	}
 }
 
 type Chain struct {
@@ -51,6 +61,11 @@ func (c *Chain) Build(r io.Reader) {
 		if !exist {
 			c.chain[p.String()] = make(map[string]int)
 		}
+		if isUpper(p.String()) {
+			c.capitalized++
+		}
+		// Increment if it is capitalized. We can use this to get the
+		// probability of the words that is capitalized.
 		c.chain[p.String()][string(b)]++
 		p.Shift(b)
 	}
@@ -58,14 +73,31 @@ func (c *Chain) Build(r io.Reader) {
 
 func (c *Chain) Generate(w io.Writer, n int) {
 	bw := bufio.NewWriter(w)
-
+	count := n
+	// Take the first key. Ensure that it is capitalized.
+	randomPrefix := rand.Intn(c.capitalized)
 	var key string
 	for key = range c.chain {
-		break
+		if !isUpper(key) {
+			continue
+		}
+		randomPrefix -= 2
+		if randomPrefix < 0 {
+			break
+		}
 	}
 	p := NewPrefix(2)
+	p.ShiftString(key)
+	bw.WriteString(key)
+	count -= 2
+
 	var total int
-	for i := 2; i < n; i++ {
+	var hasPunct bool
+	for {
+		if count < 1 && hasPunct {
+			break
+		}
+		bw.WriteString(" ")
 		total = 0
 		choices := c.chain[p.String()]
 		if len(choices) == 0 {
@@ -82,11 +114,27 @@ func (c *Chain) Generate(w io.Writer, n int) {
 			choice--
 		}
 		bw.WriteString(key)
-		bw.WriteString(" ")
 		p.Shift([]byte(key))
+		if isPunct(key) {
+			hasPunct = true
+		} else {
+			hasPunct = false
+		}
+		count -= 2
 	}
 
 	bw.Flush()
+}
+
+func isUpper(s string) bool {
+	r, _ := utf8.DecodeRuneInString(s)
+	return unicode.IsUpper(r)
+}
+
+func isPunct(s string) bool {
+	r, _ := utf8.DecodeLastRuneInString(s)
+	// return unicode.IsPunct(r)
+	return r == '.' || r == '?' || r == '!'
 }
 
 func main() {
